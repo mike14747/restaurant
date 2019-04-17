129f59019f2770d05e89c8b50477eb5e6d3257ab
mike4747 <mike4747@oh.rr.com>
2019-04-17 19:03:38 -0400

    initial file upload

---
+++ server.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+var path = require("path");
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var hasTable = [];
+var waitingList = [];
+
+app.get("/", function (req, res) {
+    // res.sendFile(path.join(__dirname, "./home.html"));
+    res.send("home.html");
+});
+
+app.get("/home", function (req, res) {
+    // res.sendFile(path.join(__dirname, "./home.html"));
+    res.send("home.html");
+});
+
+app.get("/tables", function (req, res) {
+    // res.sendFile(path.join(__dirname, "./tables.html"));
+    res.send("tables.html");
+});
+
+app.get("/reserve", function (req, res) {
+    // res.sendFile(path.join(__dirname, "./reserve.html"));
+    res.send("reserve.html");
+});
+
+app.get("*", function (req, res) {
+    res.status(404).send("<h1>This page does not exist!</h1>");
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
