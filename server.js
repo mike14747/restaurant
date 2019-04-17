93ab335b12148bfafca85647bff4fb79f98305c7
Vince216 <vinceshury@gmail.com>
2019-04-17 19:16:08 -0400

    Added start of Json response endpoints

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ app.get("/reserve", function (req, res) {
     // res.sendFile(path.join(__dirname, "./reserve.html"));
     res.send("reserve.html");
 });
+//----------------------  JSON RESPONSE ENDPOINTS-----------------------------------
+
+app.get("/api/tables", function(req, res) {
+    return res.json(hasTable);
+});
+
+app.get("/api/reserve", function(req, res) {
+    return res.json(waitingList);
+});
+
+//----------------------------------------------------------------------------------
 
 app.get("*", function (req, res) {
     res.status(404).send("<h1>This page does not exist!</h1>");
